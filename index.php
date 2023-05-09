b97498f6b75ba5a2a8b4b8ef43465fe6e9a9925a
Crackaz <82509677+Crackaz@users.noreply.github.com>
2023-05-09 14:58:48 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
-<?php
-    echo ("Hello!!");
-?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login Page</title>
+	<link rel="stylesheet" type="text/css" href="login.css">
+</head>
+<body>
+	<div class="login-box">
+		<h2>Login Here</h2>
+		<form method="post" action="login.php">
+			<p>Username:</p>
+			<input type="text" name="username" placeholder="Enter Username">
+			<p>Password:</p>
+			<input type="password" name="password" placeholder="Enter Password">
+			<input type="submit" name="submit" value="Login">
+		</form>
+	</div>
+</body>
+</html>
