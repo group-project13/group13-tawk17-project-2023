c35649063827a0cff36ea779929d44caf8e98706
Crackaz <82509677+Crackaz@users.noreply.github.com>
2023-05-09 14:59:01 +0200

    Update login.php

--- login.php
+++ login.php
@@ -1,3 +1,21 @@
 <?php
-    echo ("login");
+
+session_start();
+$con=mysqli_connect("localhost","username","password","database_name");
+
+if(isset($_POST['submit'])){
+	$username=mysqli_real_escape_string($con,$_POST['username']);
+	$password=mysqli_real_escape_string($con,$_POST['password']);
+	
+	$sql="SELECT * FROM users WHERE username='$username' AND password='$password'";
+	$result=mysqli_query($con,$sql);
+	$row=mysqli_fetch_array($result,MYSQLI_ASSOC);
+	
+	if(mysqli_num_rows($result)==1){
+		$_SESSION['username']=$username;
+		header("location: welcome.php");
+	}else{
+		echo "<script>alert('Invalid username or password.')</script>";
+	}
+}
 ?>
