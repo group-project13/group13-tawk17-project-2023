b96d01ff8e165585da20a16d81a77ffc4a6e491d
Crackaz <82509677+Crackaz@users.noreply.github.com>
2023-05-22 16:54:14 +0200

    Update home.php

--- home.php
+++ home.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<title>Table Booking Application</title>
-	<link rel="stylesheet" type="text/css" href="home.css">
+	<link rel="stylesheet" type="text/css" href="../home.css">
 </head>
 <body>
 	<header>
